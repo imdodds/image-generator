226984b57fc00a1a280a5d36783c062d07f0d722
Ian Dodds <ianmitchelldodds@gmail.com>
2023-04-12 16:02:22 -0400

    Add CreatePost page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { preview } from '../assets';
+import { getRAndomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
+
+  const navigate = useNavigate();
+
+  const [generateImg, setGenerateImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: ''
+  });
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
+
   return (
-    <div>
-      CreatePost
-    </div>
+    <section className='max-w-7xl mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[14px] max-w[500px]'>
+          Create imaginative and visually stunning images through DALL-E AI and share them with the community
+        </p>
+      </div>
+      <form
+        className='mt-16 max-w-3xl'
+        onSubmit={handleSubmit}
+      >
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder='Spongebob Squarepants in the Blair Witch Project'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
